e5cb2738debdee83661ab40667853c7c30f348ec
Gustavo Hammes <93497203+git-GMHammes@users.noreply.github.com>
2021-11-07 11:50:44 -0300

    202111071150

---
+++ instrucao31-lista.py
@@ -0,0 +1,12 @@
+#
+# Desempacotando listas
+# *_ separa o restante em uma lista diferente
+#
+lst1 = ['Mamão', 'Laraja', 'Limão', 'Goiaba', 'Tangerina']
+#
+it1, it2, it3, it4, it5 = lst1
+lt1, lt2, lt3, *_ = lst1
+#
+print(f'\n lst1 = {lst1}')
+print(f'\n it1, it2, it3, it4, it5 = lst1: it3= {it3}')
+print(f'\n lt2, lt3: {lt2, lt3}\n')
